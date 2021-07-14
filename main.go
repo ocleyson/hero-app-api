1b3cb161b8b247e61f9555715feaa1b3a3b0faa9
Cleyson L <clbbdev@gmail.com>
2021-07-14 17:26:57 -0300

    move routes from root to routes package

--- main.go
+++ main.go
@@ -3,13 +3,14 @@ package main
 import (
 	"net/http"
 
+	"github.com/ocleyson/hero-app-api/routes"
 	"github.com/ocleyson/hero-app-api/services"
 )
 
 func main() {
 	services.ConnectDatabase()
 
-	err := http.ListenAndServe(":3000", Routes())
+	err := http.ListenAndServe(":3000", routes.Routes())
 
 	if err != nil {
 		panic("Failed to serve api.")
